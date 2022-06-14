82a78ad6b2b9e0fc322ed1f0c2f4ab354320b0ac
Thomas Boni <thomas@go2scale.com>
2022-06-14 16:01:59 +0200

    feat(all): initial commit

---
+++ cli/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"encoding/json"
+	"io/fs"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/r2devops/spark"
+	"github.com/sirupsen/logrus"
+)
+
+const configPath = "conf.yaml"
+
+var logger = logrus.WithField("context", "spark_cli")
+
+// Search recursivelly for all files in a directory and return all paths
+func getFileList(dirPath string, subDirsToSkip []string) ([]string, error) {
+
+	l := logger.WithFields(logrus.Fields{
+		"action": "get_file_list",
+	})
+
+	var filesFound []string
+	sort.Strings(subDirsToSkip)
+
+	err := filepath.Walk(dirPath, func(path string, info fs.FileInfo, err error) error {
+
+		if err != nil {
+			l.WithField("path", path).WithError(err).Error("failure accessing the path")
+			return err
+		}
+
+		if info.IsDir() {
+			index := sort.SearchStrings(subDirsToSkip, info.Name())
+			if index != len(subDirsToSkip) && subDirsToSkip[index] == info.Name() {
+				l.WithField("directory", info.Name()).Debug("skipping dir without error")
+				return filepath.SkipDir
+			}
+		}
+
+		l.Debug("adding file to list: ", path)
+		filesFound = append(filesFound, path)
+		return nil
+	})
+
+	if err != nil {
+		l.WithField("path", dirPath).WithError(err).Error("error walking the path")
+		return []string{}, err
+	}
+
+	return filesFound, err
+}
+
+func main() {
+
+	// Initialize logger
+	l := logger.WithFields(logrus.Fields{
+		"action": "main",
+	})
+
+	dirsToSkip := []string{".git", "node_modules", "vendor"}
+
+	// Get file path as arg
+	if len(os.Args) < 2 {
+		l.Error("path to directory to analyze must be specified as arg")
+		os.Exit(1)
+	}
+	dirPath := os.Args[1]
+
+	// Open configuration file and get content
+	file, err := os.Open(configPath)
+	defer file.Close()
+	if err != nil {
+		l.WithError(err).Error("unable to open file")
+		os.Exit(1)
+	}
+	fileContent, err := ioutil.ReadFile(configPath)
+	conf, err := spark.ParseConf(fileContent)
+	if err != nil {
+		os.Exit(1)
+	}
+	l.WithField("conf", conf).Debug("configuration content")
+	l.Info("configuration initialized")
+
+	// Get a list of all files from the root directory
+	files, err := getFileList(dirPath, dirsToSkip)
+	if err != nil {
+		l.WithError(err).Error("error when trying to list recursivelly all files")
+		os.Exit(1)
+	}
+
+	// Analyze all techs
+	techs := map[string]*spark.LanguageData{}
+	for _, filePath := range files {
+
+		// Get the file content
+		fileContent, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			l.WithField("file", filePath).WithError(err).Warning("unable to open file")
+			continue
+		}
+
+		// Analyze file
+		fileTechs := spark.Analyze(filePath, fileContent, conf)
+		l.WithField("file", filePath).Debug("analyze of file done")
+
+		// Append it to result
+		for tech, content := range fileTechs {
+
+			if _, ok := techs[tech]; ok {
+				techs[tech].FilesMatch = append(techs[tech].FilesMatch, content.FilesMatch...)
+				techs[tech].Data = append(techs[tech].Data, content.Data...)
+			} else {
+				techs[tech] = content
+			}
+		}
+	}
+
+	// Display result
+	result, _ := json.MarshalIndent(techs, "", "   ")
+	l.Info("Analysis result: " + string(result))
+}
---
+++ spark.go
@@ -0,0 +1,141 @@
+package spark
+
+import (
+	"path"
+	"regexp"
+
+	"github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+)
+
+var logger = logrus.WithField("context", "spark")
+
+type Configuration struct {
+	Languages []struct {
+		Name      string   `yaml:"name"`
+		Filenames []string `yaml:"filenames"`
+		FileRegex []*regexp.Regexp
+		Extract   []struct {
+			Name          string `yaml:"name"`
+			File          string `yaml:"file"`
+			Regex         string `yaml:"regex"`
+			CompiledRegex *regexp.Regexp
+		} `yaml:"extract"`
+	} `yaml:"languages"`
+}
+
+type LanguageData struct {
+	FilesMatch []string
+	Data       []*ExtractedData
+}
+
+type ExtractedData struct {
+	Name     string
+	Value    string
+	FilePath string
+}
+
+func Analyze(filePath string, content []byte, conf *Configuration) map[string]*LanguageData {
+
+	// Initialize logger
+	l := logger.WithFields(logrus.Fields{
+		"action": "file_analyze",
+	})
+
+	result := make(map[string]*LanguageData, 0)
+
+	// For each languages
+	for _, language := range conf.Languages {
+
+		languageData := LanguageData{}
+
+		// Get file base nanme (without full path)
+		fileBaseName := path.Base(filePath)
+
+		// Check if one filename regex match
+		for _, filename := range language.Filenames {
+
+			// Check if filename match
+			match, err := regexp.MatchString(filename, fileBaseName)
+			if err != nil {
+				l.WithField("filename_regex", filename).WithError(err).Error("unable to parse regex, skip it")
+				continue
+			}
+			if match {
+				l.WithField("filename_regex", filename).Info("regex match !")
+				// Add matching file to
+				languageData.FilesMatch = append(languageData.FilesMatch, filePath)
+				break
+			}
+		}
+
+		// If no match, do not extract and continue to next language
+		if len(languageData.FilesMatch) == 0 {
+			continue
+		}
+
+		// Extract data
+		for _, extract := range language.Extract {
+
+			// If extraction file is the current file and complied regex exists
+			if fileBaseName == extract.File && extract.CompiledRegex != nil {
+
+				result := extract.CompiledRegex.FindSubmatch(content)
+				if result != nil && len(result) > 1 {
+					extractedData := ExtractedData{
+						Name:     extract.Name,
+						Value:    string(result[1]),
+						FilePath: filePath,
+					}
+					languageData.Data = append(languageData.Data, &extractedData)
+				} else {
+					l.WithFields(logrus.Fields{
+						"file":  extract.File,
+						"regex": extract.Regex,
+					}).Info("rexeg doesn't match")
+
+				}
+			}
+		}
+
+		// Append to existing key or create new one
+		if _, ok := result[language.Name]; ok {
+			result[language.Name].FilesMatch = append(result[language.Name].FilesMatch, languageData.FilesMatch...)
+			result[language.Name].Data = append(result[language.Name].Data, languageData.Data...)
+		} else {
+			result[language.Name] = &languageData
+		}
+	}
+
+	return result
+}
+
+func ParseConf(fileContent []byte) (*Configuration, error) {
+
+	// Initialize logger
+	l := logger.WithFields(logrus.Fields{
+		"action": "parse_conf",
+	})
+
+	var conf *Configuration
+
+	err := yaml.Unmarshal(fileContent, &conf)
+	if err != nil {
+		l.WithError(err).Error("unable to unmarshal yaml configuration file")
+		return conf, err
+	}
+
+	// Prepare compilation of all regex to extract data
+	for iLanguage, language := range conf.Languages {
+		for iExtract, extract := range language.Extract {
+			regex, err := regexp.Compile(extract.Regex)
+			if err != nil {
+				l.WithField("regex", extract.Regex).WithError(err).Error("unable to data extraction compile regex, skip it")
+				continue
+			}
+			conf.Languages[iLanguage].Extract[iExtract].CompiledRegex = regex
+			l.WithField("*regex", regex).Debug("regex is compiled !")
+		}
+	}
+	return conf, nil
+}
